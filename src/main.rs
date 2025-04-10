031afb74e4ccb875a511540fe162aefa079fb0a3
freddielumsden <freddielumsden@gmail.com>
2025-04-10 10:14:26 +0100

    before i try to implement proper 2d collision

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,43 @@
 use macroquad::prelude::*;
 
 const G: f32 = 0.09; // Gravity constant
-const DELTA: f32 = 1.0;
 const BG_COLOR: Color = BLACK;
+const GRID_SIZE: [i32; 2] = [10, 10];
 
 struct Ball {
     color: Color,
     radius: f32,
     x_pos: f32,
     y_pos: f32,
-    x_init_pos: f32, // At the start of the most recent collision
-    y_init_pos: f32,
     x_vel: f32,
     y_vel: f32,
-    x_init_vel: f32, // At the start of the most recent collision
-    y_init_vel: f32,
     x_acc: f32,
     y_acc: f32,
-    time: f32, // (relative, resets to 0 after every collision)
     boundary_cor: f32, // COR that the ball has with boundaries  https://en.wikipedia.org/wiki/Coefficient_of_restitution
 }
 
 impl Ball {
     fn update(&mut self, screen: [f32; 2]) {
-        self.time += DELTA;
-        // We know the acceleration, we know the time and we know the initial velocity
-        self.x_vel = self.x_init_vel + self.x_acc * self.time;
-        self.y_vel = self.y_init_vel + self.y_acc * self.time;
+        self.x_vel += self.x_acc;
+        self.y_vel += self.y_acc;
 
-        // We know u, v, t, a, find s (want to use most efficient equation)
-        let displacement_x = (self.x_init_vel + self.x_vel)/2.0 * self.time;
-        let displacement_y = (self.y_init_vel + self.y_vel)/2.0 * self.time;
-        self.x_pos = self.x_init_pos + displacement_x;
-        self.y_pos = self.y_init_pos + displacement_y;
-        
+        self.x_pos += self.x_vel;
+        self.y_pos += self.y_vel;
 
         // collision w boundary detection
         if self.x_pos+self.radius > screen[0] {
             self.x_vel = -self.x_vel*self.boundary_cor;
             self.x_pos = screen[0]-self.radius;
-            self.x_init_pos = self.x_pos;
-            self.y_init_pos = self.y_pos;
-            self.x_init_vel = self.x_vel;
-            self.y_init_vel = self.y_vel;
-            self.time = 0.0;
         } else if self.x_pos-self.radius < 0.0 {
             self.x_vel = -self.x_vel*self.boundary_cor;
             self.x_pos = self.radius;
-            self.x_init_pos = self.x_pos;
-            self.y_init_pos = self.y_pos;
-            self.x_init_vel = self.x_vel;
-            self.y_init_vel = self.y_vel;
-            self.time = 0.0;
         }
         if self.y_pos+self.radius > screen[1]{
             self.y_vel = -self.y_vel*self.boundary_cor;
             self.y_pos = screen[1]-self.radius;
-            self.x_init_pos = self.x_pos;
-            self.y_init_pos = self.y_pos;
-            self.x_init_vel = self.x_vel;
-            self.y_init_vel = self.y_vel;
-            self.time = 0.0;
         } else if self.y_pos-self.radius < 0.0 {
             self.y_vel = -self.y_vel*self.boundary_cor;
             self.y_pos = self.radius;
-            self.x_init_pos = self.x_pos;
-            self.y_init_pos = self.y_pos;
-            self.x_init_vel = self.x_vel;
-            self.y_init_vel = self.y_vel;
-            self.time = 0.0;
         }
         //collision w cursor detection
         let cursor = mouse_position();
@@ -76,11 +45,6 @@ impl Ball {
             if cursor.1 > self.y_pos-self.radius && cursor.1 < self.y_pos+self.radius {
                 self.y_vel = -self.y_vel;
                 self.x_vel = -self.x_vel;
-                self.x_init_pos = self.x_pos;
-                self.y_init_pos = self.y_pos;
-                self.x_init_vel = self.x_vel;
-                self.y_init_vel = self.y_vel;
-                self.time = 0.0;
             }
         }
 
@@ -105,20 +69,23 @@ fn collide(ball_1: &mut Ball, ball_2: &mut Ball) {
         ball_2.y_pos -= y_proportion * overlap;
         
 
-        // let m = (ball_1.y_pos-ball_2.y_pos)/(ball_1.x_pos-ball_2.x_pos);
+        let m = (ball_1.y_pos-ball_2.y_pos)/(ball_1.x_pos-ball_2.x_pos);
+        let theta = m.atan().abs();
+        let y_change = overlap*theta.sin();
+        let x_change = overlap*theta.cos();
 
-
-        ball_1.time = 0.0;
-        ball_2.time = 0.0;
-        ball_1.x_init_pos = ball_1.x_pos;
-        ball_2.x_init_pos = ball_2.x_pos;
-        ball_1.y_init_pos = ball_1.y_pos;
-        ball_2.y_init_pos = ball_2.y_pos;
-        ball_1.x_init_vel = ball_1.x_vel;
-        ball_2.x_init_vel = ball_2.x_vel;
-        ball_1.y_init_vel = ball_1.y_vel;
-        ball_2.y_init_vel = ball_2.y_vel;
-        
+        // Making sure that you do the adding and subtracting the right way round, don't just make the overlap larger
+        if ball_1.x_pos <= ball_2.x_pos {
+            ball_1.x_pos -= x_change;
+            ball_2.x_pos += x_change;
+            ball_1.y_pos -= y_change;
+            ball_2.y_pos += y_change;
+        } else {
+            ball_1.x_pos += x_change;
+            ball_2.x_pos -= x_change;
+            ball_1.y_pos += y_change;
+            ball_2.y_pos -= y_change;
+        }
     }
 }
 
@@ -126,60 +93,59 @@ fn collide(ball_1: &mut Ball, ball_2: &mut Ball) {
 // USE GRID SYSTEM TO MAKE MORE EFFICIENT
 #[macroquad::main("WAZZA")]
 async fn main() {
-    let color_choices = [RED, BLUE, YELLOW];
+    let color_choices = [RED, BLUE, YELLOW, ORANGE];
     let mut balls: Vec<Ball> = Vec::new();
-    for i in 0..100 {
-        for j in 0..100 {
+    for i in 0..2 {
+        for j in 0..5 {
             balls.push(Ball {
-            color: color_choices[i%3],
-            radius: 4.0,
+            color: color_choices[i%4],
+            radius: 20.0,
             x_pos: (i as f32)*((screen_width() as f32)/100.0),
             y_pos: (j as f32)*((screen_height() as f32)/100.0),
-            x_init_pos: (i as f32)*((screen_width() as f32)/100.0),
-            y_init_pos: (j as f32)*((screen_height() as f32)/100.0),
             x_vel: 30.0,
             y_vel: (i as f32),
-            x_init_vel: 30.0,
-            y_init_vel: (i as f32),
             x_acc: 0.0,
             y_acc: G,
-            time: 0.0,
             boundary_cor: 0.9,
         });
     }
     }
     loop {
+        // You have to do passes in order to have less overlap
         clear_background(BG_COLOR); //Screen is cleared whether or not function is called so no performance reduction
         let screen = [screen_width(), screen_height()];
         
         for ball in 0..balls.len() { // Physics update all balls
             balls[ball].update(screen);
         }
-        
-        for y_grid_pos in 0..10 { //10x10 grid
-            let y_bounds = [screen[1]*(y_grid_pos as f32/10.0), screen[1]*((y_grid_pos as f32 + 1.0)/10.0)];
-            for x_grid_pos in 0..10 {
-                let x_bounds = [screen[0]*(x_grid_pos as f32/10.0), screen[0]*((x_grid_pos as f32 + 1.0)/10.0)];
-                let mut grid_balls: Vec<usize> = Vec::new();
-                for ball in 0..balls.len() {
-                    if (balls[ball].x_pos+balls[ball].radius >= x_bounds[0] && balls[ball].x_pos-balls[ball].radius <= x_bounds[1]) && (balls[ball].y_pos >= y_bounds[0] && balls[ball].y_pos <= y_bounds[1]) {
-                        grid_balls.push(ball);
-                    }
+        for _ in 0..5 {
+        // Ball to ball collision detection
+            for y_grid_pos in 0..GRID_SIZE[1] { //10x10 grid
+                let y_bounds = [screen[1]*(y_grid_pos as f32/10.0), screen[1]*((y_grid_pos as f32 + 1.0)/10.0)];
+                for x_grid_pos in 0..GRID_SIZE[0] {
+                    let x_bounds = [screen[0]*(x_grid_pos as f32/10.0), screen[0]*((x_grid_pos as f32 + 1.0)/10.0)];
+                    let mut grid_balls: Vec<usize> = Vec::new();
+                    for ball in 0..balls.len() {
+                        if (balls[ball].x_pos+balls[ball].radius >= x_bounds[0] && balls[ball].x_pos-balls[ball].radius <= x_bounds[1]) && (balls[ball].y_pos+balls[ball].radius >= y_bounds[0] && balls[ball].y_pos+balls[ball].radius <= y_bounds[1]) {
+                            grid_balls.push(ball);
+                        }
 
-                }
-                for ball in 0..grid_balls.len() {
-                    for other_ball in ball+1..grid_balls.len() {
-                        let (left, right) = balls.split_at_mut(grid_balls[other_ball]);
-                        collide(&mut left[grid_balls[ball]], &mut right[0]);
+                    }
+                    for ball in 0..grid_balls.len() {
+                        for other_ball in ball+1..grid_balls.len() {
+                            let (left, right) = balls.split_at_mut(grid_balls[other_ball]);
+                            collide(&mut left[grid_balls[ball]], &mut right[0]);
+                        }
                     }
                 }
-            }
-        };
+            };
         for ball in 0..balls.len() { // Physics update all balls
             draw_circle(balls[ball].x_pos, balls[ball].y_pos, balls[ball].radius, balls[ball].color);
         }
-        draw_fps();
+        
+    }
+    draw_fps();
             
-        next_frame().await
+    next_frame().await
     }
 }

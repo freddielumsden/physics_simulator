5dbbffbae9cdafb660a7439797fd221ac8d3da73
freddielumsden <freddielumsden@gmail.com>
2025-04-21 17:55:40 +0100

    it works!!

--- src/main.rs
+++ src/main.rs
@@ -42,138 +42,64 @@ impl Ball {
                 self.vel[0] = -self.vel[0];
             }
         }
-
     }
 }
 
 fn collide(ball_1: &mut Ball, ball_2: &mut Ball) {
     // It's okay to check current frame because balls have just been updated but not displayed, so can still stop them from being inside each other for this frame
-
     let distance = ((ball_1.pos[0]-ball_2.pos[0]).powf(2.0)+(ball_1.pos[1]-ball_2.pos[1]).powf(2.0)).sqrt(); 
-    if distance < ball_1.radius + ball_2.radius {
-        /*
-        let ball_1_vels = [ball_1.vel[0], ball_1.vel[1]];
-        [ball_1.vel[0], ball_1.vel[1]] = [ball_2.vel[0], ball_2.vel[1]];
-        [ball_2.vel[0], ball_2.vel[1]] = ball_1_vels;       
-        
-        // Chat GPT generated the 7 lines of code after this one. But there was an error which after going through it mathermatically on paper I spotted. Remember this moment!!!
-        
-        
-        
-        let x_proportion = (ball_1.pos[0]-ball_2.pos[0])/distance;
-        let y_proportion = (ball_1.pos[1]-ball_2.pos[1])/distance;
-        
-        ball_1.pos[0] += x_proportion * overlap;
-        ball_1.pos[1] += y_proportion * overlap;
-        ball_2.pos[0] -= x_proportion * overlap;
-        ball_2.pos[1] -= y_proportion * overlap;
-        
-        // TODO: Optimise this maths -> trig function use
-        // Moves the balls out of each other. This is my maths btw!
-        // Collision checking logic above negates need for below code -> the balls never overlap!
-        let overlap = ((ball_1.radius + ball_2.radius) - distance)/2.0;
+    if distance <= ball_1.radius + ball_2.radius {
+        let v_1 = ball_1.vel;
+        let v_2 = ball_2.vel;
 
-        let m = (ball_1.pos[1]-ball_2.pos[1])/(ball_1.pos[0]-ball_2.pos[0]);
-        let theta = m.atan().abs();
-        let y_change = overlap*theta.sin();
-        let x_change = overlap*theta.cos();
-        
-        // Making sure that you do the adding and subtracting the right way round, don't just make the overlap larger
-        if ball_1.pos[0] <= ball_2.pos[0] {
-            ball_1.pos[0] -= x_change;
-            ball_2.pos[0] += x_change;
-            ball_1.pos[1] -= y_change;
-            ball_2.pos[1] += y_change;
-        } else {
-            ball_1.pos[0] += x_change;
-            ball_2.pos[0] -= x_change;
-            ball_1.pos[1] += y_change;
-            ball_2.pos[1] -= y_change;
-        }
-        
-        
-        */
-        
-        // TODO: Optimise this maths
-        // Wikipedia didn't work, it was this that worked in the end https://www.vobarian.com/collisions/2dcollisions2.pdf Very good document from 2009!
-        let normal_vector = ball_2.pos - ball_1.pos;
-        // To convert a vector to its normal just divide each component by the magnitude
-        // Bear in mind that powi is not deterministic -> might need to switch
-        let unit_normal = normal_vector / (normal_vector[0].powi(2) + normal_vector[1].powi(2)).powf(0.5);
-        let unit_tangent = Vec2::new(-unit_normal[1], unit_normal[0]);
-        
-        let v_1_in_norm_dir = unit_normal.dot(ball_1.vel);
-        let v_2_in_norm_dir = unit_normal.dot(ball_2.vel);
-        let v_1_in_tan_dir = unit_tangent.dot(ball_1.vel);
-        let v_2_in_tan_dir = unit_tangent.dot(ball_2.vel);
-
-        let new_v_1_in_norm_dir = v_2_in_norm_dir;
-        let new_v_2_in_norm_dir = v_1_in_norm_dir;
-
-        ball_1.vel = new_v_1_in_norm_dir * unit_normal + v_1_in_tan_dir * unit_tangent;
-        ball_2.vel = new_v_2_in_norm_dir * unit_normal + v_2_in_tan_dir * unit_tangent;
-
-        let distance = ((ball_1.pos[0]-ball_2.pos[0]).powf(2.0)+(ball_1.pos[1]-ball_2.pos[1]).powf(2.0)).sqrt();
-        
-    }   
-
-
-
+        // Resolve vels
+        ball_1.vel += (v_2 - v_1).dot(ball_2.pos - ball_1.pos) / distance / distance * (ball_2.pos - ball_1.pos);
+        ball_2.vel += (v_1 - v_2).dot(ball_1.pos - ball_2.pos) / distance / distance * (ball_1.pos - ball_2.pos);
+    }  
 }
 
-// TODO: ADD BALL ON BALL COLLISION
-// USE GRID SYSTEM TO MAKE MORE EFFICIENT
+
 #[macroquad::main("WAZZA")]
 async fn main() {
     let color_choices = [RED, BLUE, YELLOW, ORANGE];
     let mut balls: Vec<Ball> = Vec::new();
-    for i in 0..2 {
+    for i in 0..5 {
         for j in 0..5 {
             balls.push(Ball {
             color: color_choices[i%4],
             radius: 20.0,
             pos: Vec2::new((i as f32)*((screen_width() as f32)/10.0), (j as f32)*((screen_height() as f32)/10.0)),
-            vel: Vec2::new((i as f32)*10.0, (j as f32)*10.0),
+            vel: Vec2::new(i as f32, j as f32),
             acc: Vec2::new(0.0, G),
             boundary_cor: 0.9,
         });
     }
     }
     loop {
-        // You have to do passes in order to have less overlap
         clear_background(BG_COLOR); //Screen is cleared whether or not function is called so no performance reduction
         let screen = [screen_width(), screen_height()];
         
         for ball in 0..balls.len() { // Physics update all balls
             balls[ball].update(screen);
         }
+
         // Ball to ball collision detection
-            for y_grid_pos in 0..GRID_SIZE[1] { //10x10 grid
-                let y_bounds = [screen[1]*(y_grid_pos as f32/10.0), screen[1]*((y_grid_pos as f32 + 1.0)/10.0)];
-                for x_grid_pos in 0..GRID_SIZE[0] {
-                    let x_bounds = [screen[0]*(x_grid_pos as f32/10.0), screen[0]*((x_grid_pos as f32 + 1.0)/10.0)];
-                    let mut grid_balls: Vec<usize> = Vec::new();
-                    for ball in 0..balls.len() {
-                        if (balls[ball].pos[0]+balls[ball].radius >= x_bounds[0] && balls[ball].pos[0]-balls[ball].radius <= x_bounds[1]) && (balls[ball].pos[1]+balls[ball].radius >= y_bounds[0] && balls[ball].pos[1]+balls[ball].radius <= y_bounds[1]) {
-                            grid_balls.push(ball);
-                        }
-
-                    }
-                    for ball in 0..grid_balls.len() {
-                        for other_ball in ball+1..grid_balls.len() {
-                            let (left, right) = balls.split_at_mut(grid_balls[other_ball]);
-                            collide(&mut left[grid_balls[ball]], &mut right[0]);
-                        }
-                    }
-                }
-
-        for ball in 0..balls.len() { // Physics update all balls
-            draw_circle(balls[ball].pos[0], balls[ball].pos[1], balls[ball].radius, balls[ball].color);
+        for ball in 0..balls.len() {
+            for other_ball in ball+1..balls.len() {
+                let (left, right) = balls.split_at_mut(other_ball);
+                collide(&mut left[ball], &mut right[0]);
+            }
         }
-        
-    }
-    draw_fps();
-            
-    next_frame().await
+
+        let mut total_momentum = 0.0;
+        for ball in 0..balls.len() {
+            draw_circle(balls[ball].pos[0], balls[ball].pos[1], balls[ball].radius, balls[ball].color);
+
+            total_momentum += balls[ball].vel.length();
+        }
+        println!("{}", total_momentum);
+
+        draw_fps();
+        next_frame().await
     }
 }
